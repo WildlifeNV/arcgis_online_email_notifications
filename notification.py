b5bacdc1b0d1b6877ff7ca8a7031a2ad51eadd23
iholl <ian.michael.holl@gmail.com>
2022-02-18 13:49:14 -0800

    fixed data folder creation

--- notification.py
+++ notification.py
@@ -9,13 +9,17 @@ feature_layer_id = config("FEATURE_ID")
 
 gis = arcgis.GIS(None, username=config("ARCGIS_ONLINE_USERNAME"), password=config("ARCGIS_ONLINE_PASSWORD"))
 data = gis.content.get(feature_layer_id)
-
 name = data.title
 csv = data.export(title=name, export_format="csv", parameters=None, wait=True)
+
+if not os.path.exists("data"):
+    os.mkdir("data")
+
 export_path = csv.download(save_path="data")
 csv.delete()
 
 folder_path = "data/{}".format(name.replace(" ", "_").lower())
+
 if not os.path.exists(folder_path):
     os.mkdir(folder_path)
 
@@ -67,11 +71,15 @@ if os.path.exists(current_data):
     
     if compare_df == True:
         print("data is the same, no need to email or update current data")
+        clear_new_data(folder_path)
     else:
         new_records = new_df.loc[~new_df['ObjectID'].isin(current_df['ObjectID'])].copy()
-        path = os.path.join(folder_path, "new_records.csv")
-        new_records.to_csv(path)
-        send_email(path)   
+        output_path = os.path.join(folder_path, "new_records.csv")
+        new_records.to_csv(output_path)
+        send_email(output_path)
+        os.rename(new_data, current_data)
+        clear_new_data(folder_path)   
+        print("complete")
 else:
     os.rename(new_data, current_data)
     clear_new_data(folder_path)
