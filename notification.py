b52c291eb4952a50405f77148e3ac8b0d2f0ec03
iholl <ian.michael.holl@gmail.com>
2022-02-18 19:31:00 -0800

    added comments and minor updates

--- notification.py
+++ notification.py
@@ -5,36 +5,40 @@ import pandas as pd
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+# log into arcigs online account and export the required feature layer to a csv in the users arcgis online content
 feature_layer_id = config("FEATURE_ID")
-
 gis = arcgis.GIS(None, username=config("ARCGIS_ONLINE_USERNAME"), password=config("ARCGIS_ONLINE_PASSWORD"))
 data = gis.content.get(feature_layer_id)
 name = data.title
 csv = data.export(title=name, export_format="csv", parameters=None, wait=True)
 
+# check for the data directory if not create one, download the csv, and remove it
 if not os.path.exists("data"):
     os.mkdir("data")
-
 export_path = csv.download(save_path="data")
 csv.delete()
 
+# check if there is a folder based on the feature title to store the data and create one if not
 folder_path = "data/{}".format(name.replace(" ", "_").lower())
-
 if not os.path.exists(folder_path):
     os.mkdir(folder_path)
 
+# extract the csv files from the downloaded zipfile and delete the zipped file
 zipped_file = ZipFile(export_path)
 zipped_file.extractall(folder_path)
 zipped_file.close()
 os.remove(export_path)
 
+# remove all the files in a folder except for the current data (param: path to folder to remove files from)
 def clear_new_data(path):
     for clean_up in os.listdir(path):
         if not clean_up.endswith("current_data.csv"):    
             os.remove(os.path.join(folder_path, clean_up))
 
+# create an email, add the new records dataframe to the body of the email and send email
 def send_email(path):
-    df_attachment = pd.read_csv(path, index_col=0)
+    df = pd.read_csv(path, index_col=0)
+    df_attachment = df[["ObjectID", "First Name:", "Last Name:", "Email:", "Date:", "Location Specifics:", "Location Description:", "x", "y"]]
     recipients = ["ian.holl@ndow.org"]
     email_list = [elem.strip().split(",") for elem in recipients]
     
@@ -42,11 +46,13 @@ def send_email(path):
     msg["Subject"] = "Updates for {}".format(name)
     msg["From"] = "Your Friendly Neighborhood GIS folks"
 
-    html = """\
+    html = """
     <html>
       <head></head>
       <body>
-        <p>Here is a list of new records that have been add:</p>
+        <p>You are receiving this email becuase there are new submissions to the Wildlife Mortality Tracking Form./n
+        If you wish to unsubcribe from this list please notify the Nevada Department of Wildlife GIS staff.</p>
+        <p>Here is a list of new records that have been added in the past 24 hours:</p>
         {0}
       </body>
     </html>
@@ -61,19 +67,22 @@ def send_email(path):
     server.sendmail(msg["From"], email_list, msg.as_string())
     print("email successfully sent")
 
+# set the new and current data csvs
 current_data = os.path.join(folder_path, "current_data.csv")
 new_data = os.path.join(folder_path, "Mortality_Reporting_Form_0.csv")
 
+# if current data is not avaliable, create current data and remove files 
 if os.path.exists(current_data):
     current_df = pd.read_csv(current_data)
     new_df = pd.read_csv(new_data)
     compare_df = current_df.equals(new_df)
     
+    # if true remove files, else create csv, send email, update current data and remove files
     if compare_df == True:
         print("data is the same, no need to email or update current data")
-        # clear_new_data(folder_path)
+        clear_new_data(folder_path)
     else:
-        new_records = new_df.loc[~new_df['ObjectID'].isin(current_df['ObjectID'])].copy()
+        new_records = new_df.loc[~new_df["ObjectID"].isin(current_df["ObjectID"])].copy()
         output_path = os.path.join(folder_path, "new_records.csv")
         new_records.to_csv(output_path)
         send_email(output_path)
